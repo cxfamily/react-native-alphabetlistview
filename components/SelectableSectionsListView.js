32a71743dbf35653f7f7760efc72e12070d8561d
mbernardeau <mathias.bernardeau@zenika.com>
2016-05-09 16:23:59 +0200

    Fix: scrollTo undefined, as the scrollTo API of Listview changed with react-native 0.25

--- components/SelectableSectionsListView.js
+++ components/SelectableSectionsListView.js
@@ -110,12 +110,12 @@ class SelectableSectionsListView extends Component {
       var maxY = this.totalHeight - this.containerHeight + headerHeight;
       y = y > maxY ? maxY : y;
 
-      this.refs.listview.refs.listviewscroll.scrollTo({ x:0, y, animated: true });
+      this.refs.listview.scrollTo({ x:0, y, animated: true });
     } else {
       // this breaks, if not all of the listview is pre-rendered!
       UIManager.measure(this.cellTagMap[section], (x, y, w, h) => {
         y = y - this.props.sectionHeaderHeight;
-        this.refs.listview.refs.listviewscroll.scrollTo({ x:0, y, animated: true });
+        this.refs.listview.scrollTo({ x:0, y, animated: true });
       });
     }
 
